3d3c6dd16b74859e43110f18db1fa22eb28f98bd
Aryan Khadiri <aryankhadiri@gmail.com>
2019-09-26 16:11:18 -0700

    added the second form

--- Shifter/app/forms.py
+++ Shifter/app/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, PasswordField
+from wtforms import BooleanField, SubmitField, StringField, PasswordField
 
 class LoginForm(FlaskForm):
     Username = StringField("Username")
     Password = StringField("Password")
+
+class EditView(FlaskForm):
+    View = SubmitField("Edit/View Schedule")
+    AddEmpl = SubmitField("Add Employee")
