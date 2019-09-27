b9d6ec548207e5581048ff45bb251adccb9deb1d
NHWallace <NathanielHWallace@gmail.com>
2019-09-26 17:10:52 -0700

    Will be fixed by aryan

--- Shifter/app/forms.py
+++ Shifter/app/forms.py
@@ -1,10 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SubmitField, StringField, PasswordField
+from wtforms import BooleanField, SubmitField, StringField, PasswordField, IntegerField
+from wtforms.validators import ValidationError, DataRequired, Email
+
 
 class LoginForm(FlaskForm):
     Username = StringField("Username")
     Password = StringField("Password")
 
+
 class EditView(FlaskForm):
     View = SubmitField("Edit/View Schedule")
     AddEmpl = SubmitField("Add Employee")
+
+
+class EmployeeForm(FlaskForm):
+    first_name = StringField("First name", validators=[DataRequired()])
+    last_name = StringField("Last name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    phone_number = IntegerField("Phone number", validators=[DataRequired()])
