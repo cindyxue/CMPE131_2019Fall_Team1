8577f931f7cf7bd529e84e578c0ffd0b735a1748
Cindy Xue <cindyxue1405@gmail.com>
2019-11-13 12:39:50 -0800

    Add edit forms

--- Shifterapp/app/forms.py
+++ Shifterapp/app/forms.py
@@ -3,6 +3,10 @@ from wtforms import BooleanField, SubmitField, StringField, PasswordField, Integ
 from wtforms.fields import SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
+QUESTION_CHOICES = [('1', 'What\'s your mother\'s maiden name?'), 
+                    ('2', 'What\'s your favorite song?'),
+                    ('3', 'What\'s your favorite movie?'),    
+                    ]
 
 class LoginForm(FlaskForm):
     Username = StringField("Username")
@@ -38,3 +42,14 @@ class EmployeeForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     phone_number = IntegerField("Phone number", validators=[DataRequired()])
     submit = SubmitField("Submit info")
+
+class EditForm(FlaskForm):
+    name_company = StringField("Organization name", validators=[DataRequired()])
+    type_company = StringField("Organization type", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    enter_password = StringField("Password", validators=[DataRequired()])
+    re_password = PasswordField("Confirm Password", validators=[DataRequired()])
+    securityQuestion1 = SelectField("Security Question", choices=QUESTION_CHOICES, validators=[DataRequired()])
+    securityAnswer1 = StringField("Password", validators=[DataRequired()])
+    edit = SubmitField("Edit")
