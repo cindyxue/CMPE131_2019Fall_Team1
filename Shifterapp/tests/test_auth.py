3c42cd2367ed8e3db64a08a926dcb7ae38fc5ba7
NHWallace <NathanielHWallace@gmail.com>
2019-12-09 16:06:50 -0800

    more tests added, making a safety commit before attempting to modify conftest.py

--- Shifterapp/tests/test_auth.py
+++ Shifterapp/tests/test_auth.py
@@ -26,3 +26,48 @@ def test_add_organization_to_db(db):
     assert org_from_db.address == org1.address
     assert org_from_db.phone_number == org1.phone_number
 
+def test_persistent_db_between_tests(db):
+    org_from_db = Organization.query.get(1)
+    assert len(Organization.query.all()) == 1
+    assert org_from_db.name == 'Testers'
+    
+def test_add_employee_to_db(db):
+    emp1 = Employee(fname='Testy',
+                     lname='McTest',
+                     email='TestyMcTest822@gmail.com',
+                     phone_number='9998887777',
+                     organization_id='1',
+                     firsttimelogin = True)
+    emp1.setManager('Employee') # 'Employee' arg sets to False, 'Manager' arg sets to True
+    emp1.set_password(emp1.phone_number)
+    db.session.add(emp1)
+    assert len(Employee.query.all()) == 1
+    emp_from_db = Employee.query.get(1)
+    assert emp_from_db.fname == emp1.fname
+    assert emp_from_db.lname == emp1.lname
+    assert emp_from_db.email == emp1.email
+    assert emp_from_db.phone_number == emp1.phone_number
+    assert emp_from_db.organization_id == emp1.organization_id
+    assert emp_from_db.firsttimelogin == emp1.firsttimelogin
+    assert emp_from_db.manager == False
+    
+    # An employee's password should be their phone number by default, but the password
+    # in the database should be hashed.
+    
+    assert emp_from_db.password_hash != emp1.phone_number
+    
+    emp2 = Employee(fname='Sam',
+                    lname='TheManager',
+                    email='SamTheManager822@gmail.com',
+                    phone_number='8372934823',
+                    organization_id='1',
+                    firsttimelogin = True)
+    emp2.setManager('Manager')
+    emp2.set_password(emp2.phone_number)
+    db.session.add(emp2)
+    assert len(Employee.query.all()) == 2
+    manager_from_db = Employee.query.get(2)
+    assert manager_from_db.manager == True
+    
+    
+    
